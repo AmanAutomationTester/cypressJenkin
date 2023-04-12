40e47f55a82f9f5fe38e6a56e282dddad4015afb
dil-amsinghal <amsinghal@diligent.com>
2023-04-12 20:36:07 +0530

    343

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   "reporter": "mochawesome",
   "reporterOptions": {
     "reportDir": "cypress/results",
-    "overwrite": true,
+    "overwrite": false,
     "html": true,
     "json": true
   },
