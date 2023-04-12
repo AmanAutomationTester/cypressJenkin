9d106d29ccc217ee54db98a1e9b033753db5f021
dil-amsinghal <amsinghal@diligent.com>
2023-04-12 14:51:56 +0530

    33

---
+++ helper.groovy
@@ -0,0 +1,20 @@
+import groovy.json.JsonSlurper
+import groovy.io.FileType
+
+def parseJsonToCSV() {
+    def list = []
+    def dir = new File("logs")
+
+    def jsonSlurper = new JsonSlurper()
+    dir.eachFileRecurse (FileType.FILES) {
+        file -> list << file
+    }
+    def writer = new FileWriter("D:/Automation/git/cypressJenkin/fail.csv")
+
+
+    list.each {
+        def config = jsonSlurper.parse(it)      
+        writer.write(config.title + ";")
+    }
+    writer.close()
+}
