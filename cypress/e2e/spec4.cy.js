249c413cb606464d6add84295d9a319eb211fa69
BOARDBOOKS\sjain <sjain@diligent.com>
2023-04-13 18:13:48 +0530

    new files added

---
+++ cypress/e2e/spec4.cy.js
@@ -0,0 +1,15 @@
+//const { assert, expect } = require("chai")
+
+describe('spec 4', () => {
+    it('Tea', () => {
+      cy.contains("Sorry, something went wrong").should('not.exist')
+    })
+  
+    it('Coffee', () => {
+      cy.contains("Sorry, something went wrong").should('not.exist')
+    })
+  
+    it('Juice', () => {
+      cy.contains("Sorry, something went wrong").should('not.exist')
+    })
+  })
