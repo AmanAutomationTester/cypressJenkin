37deb69c8ca81db80bc1c49c62eaeeeb63b7650d
dil-amsinghal <amsinghal@diligent.com>
2023-04-18 12:59:31 +0530

    eee

--- cypress/e2e/spec1.cy.js
+++ cypress/e2e/spec1.cy.js
@@ -1,7 +1,7 @@
 describe('Verify spec 1', () => {
 
   it('selenium', () => {
-    cy.visit("https://www.selenium.dev/")
+    // cy.visit("https://www.selenium.dev/")
     cy.contains("Selenium WebDriver").should('not.exist')
   })
 
